4054054744aa14d41daf1042d0180b55fe66cd82
Domenico Tatone <1.domenicotatone@gmail.com>
2024-04-12 00:21:18 +0200

    Aggiornamento

---
+++ script/footer.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+   const footerHTML = `
+   <footer class="site-footer">
+   <div class="footer-container">
+     <div class="footer-column">
+       <a href="https://www.isoladiminorca.com/" target="_blank">Isola di Minorca</a>
+       <a href="https://www.isoladifuerteventura.com/" target="_blank">Isola di Fuerteventura</a>
+       <a href="https://www.isoladicretavacanze.com/" target="_blank">Isola di Creta Vacanze</a>
+       <a href="https://www.isoladimaltavacanze.com/" target="_blank">Isola di Malta Vacanze</a>
+       <a href="https://www.isoladirodivacanze.com/" target="_blank">Isola di Rodi Vacanze</a>
+     </div>
+     <div class="footer-column">
+       <a href="https://www.isoladimaiorca.com/" target="_blank">Isola di Maiorca</a>
+       <a href="https://www.isoladiibiza.com/" target="_blank">Isola di Ibiza</a>
+       <a href="https://www.isoladilanzarote.com/" target="_blank">Isola di Lanzarote</a>
+       <a href="https://www.isoladiformentera.com/" target="_blank">Isola di Formentera</a>
+       <a href="https://www.vacanzenelmediterraneo.com/" target="_blank">Vacanze nel Mediterraneo</a>
+     </div>
+     <div class="footer-column">
+       <a href="https://www.voglioviverecosi.com/" target="_blank">Voglio Vivere Così</a>
+       <a href="https://www.tierra.it/" target="_blank">Tierra</a>
+       <a href="https://www.menorcainfinita.com/it/home" target="_blank">Menorca Infinita</a>
+       <a href="https://www.kokulisworld.com/es/inicio" target="_blank">Kokulis World</a>
+       <a href="https://www.area54marketplace.com/" target="_blank">Area 54 Marketplace</a>
+     </div>
+     <div class="footer-column">
+       <a href="https://www.mollaretutto.com/" target="_blank">Mollar Tutto</a>
+       <a href="https://www.listooo.com/" target="_blank">Listooo</a>
+       <a href="https://www.unamelaalgiorno.com/" target="_blank">Una Mela al Giorno</a>
+       <a href="https://www.casaledeifruttiantichi.com/" target="_blank">Casale dei Frutti Antichi</a>
+     </div>
+     <div class="footer-column">
+       <a href="https://www.destinioltreconfine.com/" target="_blank">Destini Oltre Confine</a>
+       <a href="https://www.amicidiromeo.com/" target="_blank">Amici di Romeo</a>
+       <a href="https://www.cambiandoelrumbo.com/" target="_blank">Cambiando el Rumbo</a>
+       <a href="https://www.voglioviverecosiebooks.com/" target="_blank">Voglio Vivere Così Ebooks</a>
+     </div>
+   </div>
+ </footer> 
+   `;
+   document.querySelector('#container').insertAdjacentHTML('beforeend', footerHTML);
+ });
+ 
